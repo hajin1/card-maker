12dd9dd9050a8f07e61d6f5ea18ca32e95209bb6
hajin <hajin@synapsoft.co.kr>
2022-02-09 23:58:28 +0900

    app.js -> App.js 파일명 변경

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.module.css';
-import App from './app';
+import App from './App';
 import AuthService from './service/auth_service';
 
 const authService = new AuthService();
