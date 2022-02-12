cc0fa207794c816d54ab15e2a5da6a0c75745fab
hajin <hajin@synapsoft.co.kr>
2022-02-12 13:34:57 +0900

    issue/9 card preview ui 구성, 스타일링

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -7,6 +7,47 @@ import { useNavigate } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 
+export const THEME_TYPE = {
+    LIGHT: 'light',
+    DARK: 'dar',
+    COLORFUL: 'colorful'
+}
+const sample = [
+    {
+        id: 1,
+        name: 'Ellie',
+        company: 'Amazon',
+        theme: THEME_TYPE.DARK,
+        title: 'Software Engineer',
+        email: 'ellie@gmail.com',
+        message: "Don't forget to code your dream",
+        fileName: '',
+        fileURL: ''
+    },
+    {
+        id: 2,
+        name: 'Bob',
+        company: 'Meta',
+        theme: THEME_TYPE.LIGHT,
+        title: 'Senior Software Engineer',
+        email: 'bob@gmail.com',
+        message: "I love Coding.",
+        fileName: '',
+        fileURL: ''
+    },
+    {
+        id: 3,
+        name: 'Rachel',
+        company: 'Instagram',
+        theme: THEME_TYPE.COLORFUL,
+        title: 'Product Manager',
+        email: 'rachel@gmail.com',
+        message: "Design your dream",
+        fileName: '',
+        fileURL: ''
+    }
+];
+
 const Maker = ({ authService }) => {
     const navigate = useNavigate();
 
@@ -20,8 +61,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <section className={styles.section}>
-                <Editor />
-                <Preview />
+                <Editor cards={sample} />
+                <Preview cards={sample} />
             </section>
             <Footer />
         </section>
